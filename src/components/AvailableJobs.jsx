78a2d3d274b658f38c3f4c6043ce03f414e61a83
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-04-10 11:57:00 +0600

    statistics, available jobs and blogs component created

---
+++ src/components/AvailableJobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AvailableJobs = () => {
+    return (
+        <div>
+            <h1>Available Jobs</h1>
+        </div>
+    );
+};
+
+export default AvailableJobs;
