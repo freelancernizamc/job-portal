a949f149349d973999b06fcb6ca38fa0d5031ed4
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-04-10 19:59:49 +0600

    featuredJobs.json file added and data loaded in Jobs.jsx file

--- src/components/FeaturedJobs.jsx
+++ src/components/FeaturedJobs.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const FeaturedJobs = () => {
+    const [features, setFeatures] = useState([]);
+
+
+    useEffect(() => {
+        const fetchFeatures = async () => {
+            const response = await fetch('featuredJobs.json');
+            const data = await response.json();
+            setFeatures(data);
+        };
+        fetchFeatures();
+    }, []);
+
     return (
         <div className='mb-10'>
             <h1 className='text-5xl font-extrabold mt-20 mb-5'>Featured Jobs</h1>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className='grid grid-cols-2 gap-10 ml-20 mt-20'>
+                {features.map((features, index) => (
+                    <div className="card w-full bg-base-100 shadow-xl">
+                        <figure><img src={features.image} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                <p key={index}>{features.name}</p>
+
+                            </h2>
+                            <div className="card-actions justify-center">
+                                <div className="btn btn-outline">Remote</div>
+                                <div className="btn btn-outline">Full Time</div>
+                                <div className='flex mb-5'>
+                                    <div className='flex mr-10'>
+                                        <img src="/src/assets/Icons/Frame-4.png" alt='location' />
+                                        <p>Dhaka, Bangladesh</p>
+                                    </div>
+                                    <div className='flex'>
+                                        <img src="/src/assets/Icons/Frame.png" alt="salary" />
+                                        <p>Salary : 100K - 150K</p>
+                                    </div>
+                                </div>
+                                <button className='btn bg-[#4406CB]'>View Details</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                ))}
+            </div>
+            <div className='mt-10'> <button className='btn bg-[#4406CB]'>See All Jobs</button></div>
         </div>
     );
 };
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+
 
 const Jobs = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            const response = await fetch('categories.json');
+            const data = await response.json();
+            setCategories(data);
+        };
+        fetchCategories();
+    }, []);
+
     return (
         <div className='mb-10'>
             <h1 className='text-5xl font-extrabold mt-20 mb-5'>Job Category List</h1>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div>
+                <div className='grid grid-cols-4 gap-4 mt-20'>
+                    {categories.map((category, index) => (
+                        <div className="card card-compact w-full bg-base-100 shadow-xl">
+                            <figure><img src={category.image} alt="Jobs" /></figure>
+                            <div className="card-body">
+                                <p key={index}>{category.name}</p>
+                                <p>{category.available_jobs} Available Jobs</p>
+
+                            </div>
+                        </div>
+
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
